7e606b9915fb6291ecf4c90cb6194ef9c099efa7
Simeon Leifer <simeon@droolingcat.com>
2018-08-26 10:03:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# imager-munger
+
+A tool I wrote to generate images in various forms used in iOS apps from a set of master images, driven by manifest files.
+
+Publishing in 2021 so I can share a few tools.
+
+Released under the MIT License.
---
+++ image-munger/main.swift
@@ -0,0 +1,98 @@
+//
+//  main.swift
+//  image-munger
+//
+//  Created by Simeon Leifer on 8/24/18.
+//  Copyright © 2018 droolingcat.com. All rights reserved.
+//
+
+import Foundation
+import CommandLineCore
+
+let toolVersion = "0.1"
+var baseDirectory: String = ""
+var commandName: String = ""
+
+func main() {
+    autoreleasepool {
+        let parser = ArgParser(definition: makeCommandDefinition())
+
+        do {
+            #if DEBUG
+            let args = ["imp", "./testing/manifest.txt"]
+            commandName = args[0]
+            let parsed = try parser.parse(args)
+            #else
+            let parsed = try parser.parse(CommandLine.arguments)
+            commandName = CommandLine.arguments[0].lastPathComponent
+            #endif
+
+            baseDirectory = FileManager.default.currentDirectoryPath
+
+            if let cmd = commandFrom(parser: parser), parser.args.count > 1 {
+                cmd.run(cmd: parsed)
+            }
+        } catch {
+            print("Invalid arguments.")
+            parser.printHelp()
+        }
+
+        CommandLineRunLoop.shared.waitForBackgroundTasks()
+    }
+}
+
+// swiftlint:disable cyclomatic_complexity
+
+func commandFrom(parser: ArgParser) -> Command? {
+    var skipSubcommand = false
+    var cmd: Command?
+    let parsed = parser.parsed
+
+    if parsed.option("--version") != nil {
+        print("Version \(toolVersion)")
+        skipSubcommand = true
+    }
+    if parsed.option("--help") != nil {
+        parser.printHelp()
+        skipSubcommand = true
+    }
+
+    if skipSubcommand == false {
+        switch parsed.subcommand ?? "root" {
+        case "bashcomp":
+            cmd = BashcompCommand(parser: parser)
+        case "bashcompfile":
+            cmd = BashcompfileCommand()
+        case "process":
+            cmd = ProcessCommand()
+        case "root":
+            if parsed.parameters.count > 0 {
+                print("Unknown command: \(parsed.parameters[0])")
+            }
+        default:
+            print("Unknown command.")
+        }
+    }
+
+    return cmd
+}
+
+// swiftlint:enable cyclomatic_complexity
+
+func baseSubPath(_ subpath: String) -> String {
+    var path = subpath.standardizingPath
+    if path.isAbsolutePath == false {
+        path = baseDirectory.appendingPathComponent(path)
+    }
+    return path
+}
+
+func setCurrentDir(_ subpath: String) {
+    FileManager.default.changeCurrentDirectoryPath(baseSubPath(subpath))
+}
+
+func resetCurrentDir() {
+    setCurrentDir(baseDirectory)
+}
+
+main()
